e0cc02210a0a6595efd2bf1e5c29d9dd6323065a
Alok Kumar <helloalokkumar108@gmail.com>
2026-01-08 12:57:01 +0530

    Fix import case sensitivity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./components/auth/login";
-import Signup from "./components/auth/signup";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
 import Jobs from "./components/Jobs";
 import Browse from "./components/Browse";
